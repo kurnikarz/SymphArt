146ee2bd3274ef08d21e1b69e52452fe3688cd0a
Artur Dymkowski <arturdymkowski@wp.pl>
2020-02-24 11:11:17 +0100

    Odcinek 1

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    class ArticleController extends AbstractController{
+        /**
+         * @Route("/")
+         * @Method({"GET"})
+         */
+        public function  index() {
+            //return new Response('<html><body>Hello Worltd</body></html>');
+            $articles = ['Article1', 'Article2'];
+            return $this->render('articles/index.html.twig', array('articles' => $articles));
+        }
+    }
